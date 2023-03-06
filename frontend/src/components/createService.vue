966a98d725cfbd16e3f8faa8e3c6b4dd31adb203
tlbussey <89703127+tlbussey@users.noreply.github.com>
2023-03-06 14:59:51 +0000

    now the services page should be there

---
+++ frontend/src/components/createService.vue
@@ -0,0 +1,156 @@
+<script>
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+import axios from 'axios'
+const apiURL = import.meta.env.VITE_ROOT_API
+
+export default {
+  setup() {
+    return { v$: useVuelidate({ $autoDirty: true }) }
+  },
+  data() {
+    return {
+      // removed unnecessary extra array to track services
+      event: {
+        name: '',
+        services: [],
+        date: '',
+        address: {
+          line1: '',
+          line2: '',
+          city: '',
+          county: '',
+          zip: ''
+        },
+        description: ''
+      }
+    }
+  },
+  methods: {
+    async handleSubmitForm() {
+      // Checks to see if there are any errors in validation
+      const isFormCorrect = await this.v$.$validate()
+      // If no errors found. isFormCorrect = True then the form is submitted
+      if (isFormCorrect) {
+        axios
+          .post(`${apiURL}/events`, this.event)
+          .then(() => {
+            alert('Service has been added.')
+            this.$router.push({ name: 'findevents' })
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      }
+    }
+  },
+  // sets validations for the various data properties
+  validations() {
+    return {
+      event: {
+        name: { required },
+        date: { required }
+      }
+    }
+  }
+}
+</script>
+<template>
+  <main>
+    <div>
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+      >
+        Create New Service
+      </h1>
+    </div>
+    <div class="px-10 py-20">
+      <!-- @submit.prevent stops the submit event from reloading the page-->
+      <form @submit.prevent="handleSubmitForm">
+        <!-- grid container -->
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+        >
+          <h2 class="text-2xl font-bold">Service Details</h2>
+
+          <!-- form field -->
+          <div class="flex flex-col">
+            <label class="block">
+              <span class="text-gray-700">Service Name</span>
+              <span style="color: #ff0000">*</span>
+              <input
+                type="text"
+                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                v-model="event.name"
+              />
+              <span class="text-black" v-if="v$.event.name.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.event.name.$errors"
+                  :key="error.$uid"
+                >
+                  {{ error.$message }}!
+                </p>
+              </span>
+            </label>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div class="flex justify-between mt-10 mr-20">
+          <button class="bg-red-700 text-white rounded" type="submit">
+            Add New Service
+          </button>
+        </div>
+      </form>
+    </div>
+
+<!--UPDATE SERVICES need to add a select services which will pull from the current services and the implement the changes-->
+    <div>
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+      >
+        Update Service
+      </h1>
+    </div>
+    <div class="px-10 py-20">
+      <!-- @submit.prevent stops the submit event from reloading the page-->
+      <form @submit.prevent="handleSubmitForm">
+        <!-- grid container -->
+        <div
+          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+          <h2 class="text-2xl font-bold">Select Service</h2>
+          <!-- form field -->
+          <div class="flex flex-col">
+            <label class="block">
+              <span class="text-gray-700">Service Name</span>
+              <span style="color: #ff0000">*</span>
+              <input
+                type="text"
+                class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                v-model="event.name"
+              />
+              <span class="text-black" v-if="v$.event.name.$error">
+                <p
+                  class="text-red-700"
+                  v-for="error of v$.event.name.$errors"
+                  :key="error.$uid"
+                >
+                  {{ error.$message }}!
+                </p>
+              </span>
+            </label>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+        <div class="flex justify-between mt-10 mr-20">
+          <button class="bg-red-700 text-white rounded" type="submit">
+            Update Service
+          </button>
+        </div>
+      </form>
+    </div> 
+  </main>
+</template>
