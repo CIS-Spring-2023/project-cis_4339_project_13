ad6af2e5c51aede2e084d162939718aea40a8805
tlbussey <89703127+tlbussey@users.noreply.github.com>
2023-04-24 15:50:50 +0000

    services backend

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const org = process.env.ORG
+const services = process.env.ORG
 
 // importing data model schemas
 const { services } = require('../models/models')
 
-// GET 10 most recent services for org
+// GET 10 most recent services
 router.get('/', (req, res, next) => {
   services
-    .find({ org: org }, (error, data) => {
+    .find({ services: services }, (error, data) => {
       if (error) {
         return next(error)
       } else {
@@ -17,35 +17,21 @@ router.get('/', (req, res, next) => {
       }
     })
     // sort by date ascending
-    .sort({ date: 1 })
+    .sort({ name: 1 })
     .limit(10)
-})
-
-// GET single service by ID
-router.get('/id/:id', (req, res, next) => {
-  // use findOne instead of find to not return array
-  services.findOne({ _id: req.params.id }, (error, data) => {
-    if (error) {
-      return next(error)
-    } else if (!data) {
-      res.status(400).send('service not found')
-    } else {
-      res.json(data)
-    }
-  })
 })
 
 // GET services based on search query
 // Ex: '...?name=Food&searchBy=name'
 router.get('/search/', (req, res, next) => {
-  const dbQuery = { org: org }
+  const dbQuery = { services: services }
   switch (req.query.searchBy) {
     case 'name':
       // match service name, no anchor
       dbQuery.name = { $regex: `${req.query.name}`, $options: 'i' }
       break
-    case 'date':
-      dbQuery.date = req.query.serviceDate
+    case 'name':
+      dbQuery.name = req.query.serviceName
       break
     default:
       return res.status(400).send('invalid searchBy')
@@ -59,36 +45,10 @@ router.get('/search/', (req, res, next) => {
   })
 })
 
-// GET services for which a client is signed up
-router.get('/client/:id', (req, res, next) => {
-  services.find({ attendees: req.params.id, org: org }, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      res.json(data)
-    }
-  })
-})
-
-// GET org service attendance for the past two months
-router.get('/attendance', (req, res, next) => {
-  const twoMonthsAgo = new Date()
-  twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2)
-  services
-    .find({ org: org, date: { $gte: twoMonthsAgo } }, (error, data) => {
-      if (error) {
-        return next(error)
-      } else {
-        res.json(data)
-      }
-    })
-    .sort({ date: 1 })
-})
-
 // POST new service
 router.post('/', (req, res, next) => {
   const newservice = req.body
-  newservice.org = org
+  newservice.services = services
   services.create(newservice, (error, data) => {
     if (error) {
       return next(error)
@@ -107,52 +67,6 @@ router.put('/update/:id', (req, res, next) => {
       res.json(data)
     }
   })
-})
-
-// PUT add attendee to service
-router.put('/register', (req, res, next) => {
-  services.find(
-    { _id: req.query.service, attendees: req.query.client },
-    (error, data) => {
-      if (error) {
-        return next(error)
-      } else {
-        // only add attendee if not yet signed up
-        if (!data.length) {
-          services.findByIdAndUpdate(
-            req.query.service,
-            { $push: { attendees: req.query.client } },
-            (error, data) => {
-              if (error) {
-                console.log(error)
-                return next(error)
-              } else {
-                res.send('Client added to service')
-              }
-            }
-          )
-        } else {
-          res.status(400).send('Client already added to service')
-        }
-      }
-    }
-  )
-})
-
-// PUT remove attendee from service
-router.put('/deregister', (req, res, next) => {
-  services.findByIdAndUpdate(
-    req.query.service,
-    { $pull: { attendees: req.query.client } },
-    (error, data) => {
-      if (error) {
-        console.log(error)
-        return next(error)
-      } else {
-        res.send('Client deregistered with service')
-      }
-    }
-  )
 })
 
 // hard DELETE service by ID, as per project specifications
