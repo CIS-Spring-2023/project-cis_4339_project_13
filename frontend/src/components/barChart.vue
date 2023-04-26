233d87ff38df44a3b9a1a76adc19525273443650
Hamra Hussain <98126683+Hamra99@users.noreply.github.com>
2023-04-27 00:20:41 +0500

    update barchart

--- frontend/src/components/barChart.vue
+++ frontend/src/components/barChart.vue
@@ -1,21 +1,25 @@
 <script>
 import { Chart, registerables } from 'chart.js'
+import axios from 'axios'
 Chart.register(...registerables)
 
 export default {
   async mounted() {
+    const response = await axios.get('mongodb+srv://4339ProjectAdmin:Ai9HA8ISkHsBkgh2@cluster0.gpikkjp.mongodb.net/4339_Project_Database')
+    const data = response.data
+
     const backgroundColor = ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)']
     const borderColor = ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)']
     await new Chart(this.$refs.attendanceChart, {
       type: 'pie',
       data: {
-        labels: ['Red', 'Blue', 'Yellow'],
+        labels: data.labels,
         datasets: [
           {
             borderWidth: 1,
             backgroundColor: backgroundColor,
             borderColor: borderColor,
-            data: [10, 20, 30]
+            data: data.values
           }
         ]
       },
